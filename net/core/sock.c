457e3fabcc7d36c717540ca4eab21e5d720d280e
Siddharth Santurkar <ssanturk@andrew.cmu.edu>
2016-05-04 01:13:10 -0400

    callback debug fix

--- net/core/sock.c
+++ net/core/sock.c
@@ -3168,7 +3168,6 @@ void cl_timer_callback( unsigned long data ) {
 	cl_list_node *curr, *next;
 
 	printk("cl_timer_callback: called\n");
-	printk("cl_timer_callback: before progress(%d)\n", progress);
 	// 0) Try to acquire right to traverse
 	// int atomic_cmpxchg(atomic_t *v, int old, int new);
 	int progress = atomic_read(&sock_list.xfer_in_progress);
