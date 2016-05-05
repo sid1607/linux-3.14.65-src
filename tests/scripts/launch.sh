c5f1ba70ec38a8f1d9f2e92900130084fe0d9739
Siddharth Santurkar <ssanturk@andrew.cmu.edu>
2016-05-04 23:39:46 -0400

    refactored tcp test

--- tests/scripts/launch.sh
+++ tests/scripts/launch.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+srcdir="$(dirname "$0")"
+
 # A POSIX variable
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
 args_parsed=
@@ -12,28 +14,34 @@ delay_ms=0
 
 dump_packets=
 
-ip_to_send=""
-port_to_send=""
+client_program="$srcdir/client.py"
+server_program="$srcdir/server.py"
 
-function show_help {
+program_to_run=""
+
+ip_to_send="localhost"
+port_to_send="15744"
+
+show_help() {
     echo "Usage:" >&2
     echo "  -h  Show help (this)" >&2
     echo "  -n  Number of packets to send (default: 1024)" >&2
     echo "  -d  Delay tolerance option in milliseconds (default: off)" >&2
-    echo "  -a  Destination IP address" >&2
-    echo "  -p  Destination port" >&2
+    echo "  -a  Destination IP address (default: localhost)" >&2
+    echo "  -p  Destination port (default: 15744)" >&2
+    echo "  -r  Program to run (values: client, server)" >&2
     echo "  -s  Dump packets (default: off) (requires superuser privileges)" >&2
 }
 
-function print_options {
+print_options() {
     echo "Destination: $ip_to_send:$port_to_send" >&2
     echo "Number of packets to send: $num_packets" >&2
-    if [[ "$set_delay" == true ]]; then
+    if [ "$set_delay" = true ]; then
         echo "Application delay: $delay_ms ms" >&2
     else
         echo "Application delay: off" >&2
     fi
-    if [[ "$dump_packets" == true ]]; then
+    if [ "$dump_packets" = true ]; then
         echo "Packet dump: on" >&2
     else
         echo "Packet dump: off" >&2
@@ -42,7 +50,7 @@ function print_options {
 }
 
 # Parse arguments
-options=":hn:d:a:p:s"
+options=":hn:d:a:p:s:r:"
 
 while getopts $options opt; do
     case "$opt" in
@@ -58,6 +66,16 @@ while getopts $options opt; do
         ;;
     p)  port_to_send=$OPTARG
         ;;
+    r)  if [ "$OPTARG" = "client" ]; then
+            program_to_run=$client_program
+        elif [ "$OPTARG" = "server" ]; then
+            program_to_run=$server_program
+        else
+            echo "Invalid program: $OPTARG"
+            show_help
+            exit 1
+        fi
+        ;;
     s)  dump_packets=true
         ;;
     \?) echo "Invalid option: -$OPTARG" >&2
@@ -71,10 +89,17 @@ while getopts $options opt; do
     esac
 done
 
-# Check if required arguments are provided
-if [[ "$ip_to_send" == "" ]] || [[ "$port_to_send" == "" ]]; then
+# Check if a program has been specified
+if [ "$program_to_run" = "" ]; then
+    echo "Specify a program to run (client or server)"
     show_help
-    exit 0
+    exit 1
+fi
+
+# Check if required arguments are provided
+if [ "$ip_to_send" = "" ] || [ "$port_to_send" = "" ]; then
+    show_help
+    exit 1
 else
     args_parsed=true
 fi
@@ -87,5 +112,9 @@ print_options
 #     tcpdump -q -nn -w test_dump.pcap tcp and dst port $port_to_send and dst host $ip_to_send
 # fi
 
-# Start the transfer
-python Sender.py -a "$ip_to_send" -p $port_to_send -n $num_packets -d $delay_ms
+# Start the program
+if [ "$program_to_run" = "$client_program" ]; then
+    python $program_to_run -a $ip_to_send -p $port_to_send -n $num_packets -d $delay_ms
+else
+    python $program_to_run -a $ip_to_send -p $port_to_send -d 10000
+fi
