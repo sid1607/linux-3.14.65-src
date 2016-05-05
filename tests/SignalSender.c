10e63d27074852054ee9ca06cb5c9074d92f1f86
A. Emre Ünal <aemreunal@gmail.com>
2016-05-04 22:49:08 -0400

    Add sleep wrapper

--- tests/SignalSender.c
+++ tests/SignalSender.c
@@ -1,32 +1,48 @@
 #include <signal.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 #include <unistd.h>
 
 const int minSleepMillis = 100;
 const int maxSleepMillis = 300;
 
-const int MILLIS_TO_MICROS = 1000;
+const int MILLIS_TO_NANOS = 1000000;
 const int KERNEL_PID = 1;
 const int TEST_SIGNAL = 100;
+struct timespec sleepDuration={0};
+
+void msleep(unsigned long milisec);
 
 int main(int argc, char const *argv[]) {
 	// Seed the random number generator
 	srand(time(NULL));
+
 
 	while(1) {
 		// Determine sleep time
 		int sleepMillis = (rand() % (maxSleepMillis - minSleepMillis)) + minSleepMillis;
 
 		// Sleep for that amount
-		usleep(sleepMillis * MILLIS_TO_MICROS);
+		msleep((long) sleepMillis);
 
 		// Send signal to PID
 		// https://www.win.tue.nl/~aeb/linux/lk/lk-5.html
 		// http://www.gnu.org/software/libc/manual/html_node/Signaling-Another-Process.html
-		// int kill(pid_t pid, int sig);
-		kill(KERNEL_PID, TEST_SIGNAL);
+		// kill(KERNEL_PID, TEST_SIGNAL);
+		printf("Slept for %d.\n", sleepMillis);
 	}
 
 	return 0;
+}
+
+// http://cc.byexamples.com/2007/05/25/nanosleep-is-better-than-sleep-and-usleep/
+void msleep(unsigned long milisec) {
+    time_t sec=(int)(milisec/1000);
+    milisec=milisec-(sec*1000);
+    sleepDuration.tv_sec=sec;
+    sleepDuration.tv_nsec=milisec*1000000L;
+    while(nanosleep(&sleepDuration, &sleepDuration)==-1) {
+        continue;
+    }
 }
